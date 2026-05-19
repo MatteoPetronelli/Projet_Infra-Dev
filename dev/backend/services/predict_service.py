4df23499b719e0d95d4371e7b14e065b33324911
MatteoPetronelli <matteo.petronelli5@gmail.com>
2026-05-19 14:50:20 +0200

    adding resilient behaviour for charging IA module

--- dev/backend/services/predict_service.py
+++ dev/backend/services/predict_service.py
@@ -1,11 +1,20 @@
 import joblib
+import os
 import numpy as np
+from core.logger import logger
 
 class PredictService:
     def __init__(self, model_path: str):
-        self.model = joblib.load(model_path)
+        if os.path.exists(model_path):
+            self.model = joblib.load(model_path)
+            logger.info("Modèle IA chargé avec succès.")
+        else:
+            self.model = None
+            logger.warning(f"Modèle IA introuvable à {model_path}. La prédiction est désactivée.")
 
     def get_prediction(self, data: dict) -> float:
+        if self.model is None:
+            raise RuntimeError("Le modèle IA n'est pas disponible sur ce serveur.")
         features = np.array([[
             data['surface_reelle_bati'],
             data['nombre_pieces_principales'],
